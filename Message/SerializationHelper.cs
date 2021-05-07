d7707983e8fda31ab68869e534b62b350bdbbc85
ta.nguyen <ta.nguyen@TANGUYEN>
2021-05-07 14:19:34 +0700

    update

--- Message/SerializationHelper.cs
+++ Message/SerializationHelper.cs
@@ -6,27 +6,6 @@ namespace Message
 {
     public static class SerializationHelper
     {
-        public static byte[] ToByteArray<T>(this T t)
-        {
-            using (var memoryStream = new MemoryStream())
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
-                formatter.Serialize(memoryStream, t);
-                return memoryStream.ToArray();
-            }
-        }
-
-        public static object FromByteArray(byte[] data)
-        {
-            using (MemoryStream stream = new MemoryStream())
-            {
-                stream.Write(data, 0, data.Length);
-                stream.Position = 0;
-                BinaryFormatter formatter = new BinaryFormatter();
-                return formatter.Deserialize(stream);
-            }
-        }
-
         public static T ConvertToObject<T>(string json)
         {
             try
